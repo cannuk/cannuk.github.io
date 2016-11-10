f273fede2a48396a7d8ba72dddc523271a81c65f
Sean Neden <sean.neden+github@gmail.com>
2016-11-10 10:40:48 -0800

    Update all.js

--- javascripts/all.js
+++ javascripts/all.js
@@ -20479,7 +20479,7 @@ Kinetic.Node.addGettersSetters(Kinetic.TextPath, ['fontFamily', 'fontSize', 'fon
       return Weather.__super__.constructor.apply(this, arguments);
     }
 
-    Weather.prototype.urlRoot = "http://query.yahooapis.com/v1/public/yql?q=select%20wind%2C%20astronomy%2C%20item.condition.code%2C%20item.condition.temp%2C%20item.condition.text%20%20%20from%20weather.forecast%20where%20location%3D%2294105%22&format=json&callback=?";
+    Weather.prototype.urlRoot = "http://query.yahooapis.com/v1/public/yql?q=select%20wind%2C%20astronomy%2C%20item.condition.code%2C%20item.condition.temp%2C%20item.condition.text%20%20%20from%20weather.forecast%20where%20woeid%3D2487956&format=json&callback=?";
 
     Weather.prototype.initialize = function() {
       this.setScene();
